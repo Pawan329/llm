389fddc91d3a0b7b718406f645d7d2606ac636ee
pawan329 <pawan.sviet@gmail.com>
2026-05-27 23:59:56 +0530

    added history function in chatbot

--- chat_models/chatbot.py
+++ chat_models/chatbot.py
@@ -1,13 +1,21 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.9)
 
+chat_history = [
+    SystemMessage(content="You are a helpful assistant.")
+]
+
 while True:
     user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() in ["exit", "quit"]:
         print("Exiting the chatbot. Goodbye!")
         break
-    response = model.invoke(user_input)
+    response = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=response.content))
     print(f"AI: {response.content}")
+print(chat_history)
